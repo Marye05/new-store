685c54bcce9095894e5a613e04aaa1646df5a478
m.orozco <m.orozco@tiendasdaka.com>
2025-09-17 12:07:49 -0400

    fix: update files

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,40 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { TasksService } from "./tasks.service";
+import { parse } from "path";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
-@Controller({})
+@Controller('/tasks')
 export class TasksController {
+    constructor(private tasksService: TasksService) {}
 
-    @Get('/tasks')
+    @Get()
     getAllTasks() {
-        return "This action returns all tasks";
+        return this.tasksService.getTasks();
+    }
+
+    @Get('/:id')
+    getTask(@Param('id') id: string) {
+        return this.tasksService.getTask(parseInt(id));
+    }
+
+    @Post()
+    createTasks(@Body() task: CreateTaskDto) {
+        return this.tasksService.createTask(task);
+    }
+
+    @Put()
+    updateTasks(@Body() task: UpdateTaskDto) {
+        return this.tasksService.updateTask(task);
+    }
+
+    @Delete()
+    deleteTasks() {
+        return this.tasksService.deleteTask();
+    }
+
+    @Patch()
+    updateTasksStatus() {
+        return this.tasksService.updateTaskStatus();
     }
 }
